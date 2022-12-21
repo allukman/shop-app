224c1209b0b1cad710351e479eadad7f2d8cb208
Al Lukman <allukman1802@gmail.com>
2022-12-21 14:13:40 +0700

    add drawer, screen order and expandable item

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,11 @@ import 'package:provider/provider.dart';
 import '../screens/product_overview_screen.dart';
 import '../screens/product_detail_screen.dart';
 import '../screens/cart_screen.dart';
+import '../screens/orders_screen.dart';
+
 import '../providers/products_provider.dart';
 import '../providers/cart.dart';
+import '../providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,6 +23,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (ctx) => Cart(),
         ),
+        ChangeNotifierProvider(
+          create: (ctx) => Orders(),
+        ),
       ],
       child: MaterialApp(
         title: 'MyShop',
@@ -31,6 +37,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
         },
       ),
     );
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
 import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
@@ -29,7 +30,7 @@ class CartScreen extends StatelessWidget {
                   Spacer(),
                   Chip(
                     label: Text(
-                      '\$${cart.totalAmount}',
+                      '\$${cart.totalAmount.toStringAsFixed(2)}',
                       style: TextStyle(
                           color: Theme.of(context)
                               .primaryTextTheme
@@ -39,7 +40,11 @@ class CartScreen extends StatelessWidget {
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.items.values.toList(), cart.totalAmount);
+                      cart.clear();
+                    },
                     child: Text("ORDER NOW"),
                     style: TextButton.styleFrom(
                         foregroundColor: Theme.of(context).primaryColor),
